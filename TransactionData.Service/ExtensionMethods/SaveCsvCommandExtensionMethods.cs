36337450408182a69d38787cd0f94cc21c9689b0
Artur Zayats <azayats@exoft.net>
2020-07-19 19:01:24 +0300

    Added bad rows logging

--- TransactionData.Service/ExtensionMethods/SaveCsvCommandExtensionMethods.cs
+++ TransactionData.Service/ExtensionMethods/SaveCsvCommandExtensionMethods.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using CSharpFunctionalExtensions;
 using CsvHelper;
+using Microsoft.Extensions.Logging;
 using TransactionData.Domain.Commands;
 using TransactionData.Domain.Models;
 using TransactionData.Service.CsvMap;
@@ -19,8 +20,27 @@ namespace TransactionData.Service.ExtensionMethods
             {
                 using TextReader reader = new StreamReader(command.Stream);
                 using var csv = new CsvReader(reader, CultureInfo.CurrentCulture);
+                var goodRows = new List<CsvTransactionModel>();
+                var badRows = new List<string>(); 
                 csv.Configuration.RegisterClassMap<CsvTransactionMap>();
-                return csv.GetRecords<CsvTransactionModel>().ToList();
+                while (csv.Read())
+                {
+                    try
+                    {
+                        var record = csv.GetRecord<CsvTransactionModel>();
+                        goodRows.Add(record);
+                    }
+                    catch (Exception e)
+                    {
+                        badRows.Add($"{e.InnerException?.Message ?? e.Message}\nBroken row:\n{csv.Context.RawRecord}");
+                    }
+                }
+
+                if (badRows.Any())
+                    return Result.Failure<List<CsvTransactionModel>>(badRows.Aggregate("",
+                        (first, second) => $"{first}{second}\n"));
+
+                return goodRows;
             }
             catch (Exception e)
             {
